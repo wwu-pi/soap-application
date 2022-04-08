989b4615938af48803e883dba45caa3b1e358288
Hendrik Winkelmann <hendrik.winkelmann@uni-muenster.de>
2022-04-08 14:17:53 +0200

    Wording

--- soap-client/src/main/java/de/wwu/acse/soap/client/AdderSoapClient.java
+++ soap-client/src/main/java/de/wwu/acse/soap/client/AdderSoapClient.java
@@ -11,18 +11,18 @@ import de.wwu.acse.soap.soap_generated.AddResponse;
 
 @Component
 public class AdderSoapClient extends WebServiceGatewaySupport {
-	private final String adderWebServiceUrl;
+	private final String adderWebServiceUri;
 	
 	@Autowired
-	public AdderSoapClient(@Value("${adderws.url}") String url) {
-		this.adderWebServiceUrl = url;
+	public AdderSoapClient(@Value("${adderws.uri}") String uri) {
+		this.adderWebServiceUri = uri;
 		// The following configurations are used by WebServiceTemplate to 
 		// automatically (de-)marshall the SOAP messags.
 		// Point marshaller to generated classes for (de-)marshalling of SOAP-messages 
 		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
 		// this package must match the package in the <generatePackage> specified in pom.xml
 		marshaller.setContextPath("de.wwu.acse.soap.soap_generated");
-		this.setDefaultUri(adderWebServiceUrl);
+		this.setDefaultUri(adderWebServiceUri);
 		// Set marshaller for this client
 		this.setMarshaller(marshaller);
 		this.setUnmarshaller(marshaller);
